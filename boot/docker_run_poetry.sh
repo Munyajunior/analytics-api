9db3cb8146916ff688bdb2d0294fc741ca86ddc4
munyajunior <ivojunior671@gmail.com>
2025-07-30 04:38:05 +0100

    chore: Refactor Dockerfile and update run script
    This commit refactors the Dockerfile for clarity, efficiency, and
    improved layer caching.  It streamlines the installation process
    for Poetry and project dependencies.
    
    The docker_run_poetry.sh script is updated to handle default host/port
    values and uses `exec` to replace the shell process with gunicorn.
    
    A .dockerignore file is added to exclude unnecessary files.

--- boot/docker_run_poetry.sh
+++ boot/docker_run_poetry.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-source /opt/venv/bin/activate
+# Set default values for HOST and PORT if not provided
+RUN_PORT="${PORT:-8000}"
+RUN_HOST="${HOST:-0.0.0.0}"
 
+# Move into project directory
 cd /code
-RUN_PORT=${PORT:=8000}
-RUN_HOST=${HOST:=0.0.0.0}
 
-
-poetry run gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:$RUN_PORT main:app
+# Run the app using Poetry + Gunicorn with Uvicorn worker
+exec poetry run gunicorn -k uvicorn.workers.UvicornWorker -b "$RUN_HOST:$RUN_PORT" main:app
